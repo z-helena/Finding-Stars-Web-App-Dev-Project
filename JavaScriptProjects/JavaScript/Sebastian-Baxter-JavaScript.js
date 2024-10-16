ef00571b4b2add6887d79697a1e77a1270d96c25
Sebastian Baxter <sebastian_baxter@horacemann.org>
2024-10-16 12:32:25 -0400

    final commits for javascript project

--- JavaScriptProjects/JavaScript/Sebastian-Baxter-JavaScript.js
+++ JavaScriptProjects/JavaScript/Sebastian-Baxter-JavaScript.js
@@ -1,7 +1,10 @@
 
+
+//Idnetifying the bottom text
 let bottomText = document.getElementById("bottomText");
 bottomText.style.display = "none";
 
+//Making the bottom text apear or dissapear when the button is clicked
 const MakeAppearButton = document.getElementById("bottomTextButton");
 MakeAppearButton.addEventListener("click", function(e){
     let o = document.getElementById("bottomText");
@@ -12,14 +15,27 @@ MakeAppearButton.addEventListener("click", function(e){
       }
 })
 
+//making the h4 element increase when clicked
 const H4TextButton = document.getElementById("h4TextButton");
 H4TextButton.addEventListener("click", function(f){
     let p = document.getElementById("h4Text");
     p.innerText++;
 })
 
+//And decrease with another button
+const H4TextButtonSmall = document.getElementById("h4TextButtonSmall");
+H4TextButtonSmall.addEventListener("click", function(k){
+    let p = document.getElementById("h4Text");
+    p.innerText--;
+})
+
+
+//The text to change the color of
 let q = document.getElementById("colorP");
 
+//The buttons to change the color on click
+
+//Aquamarine
 const aquamarineButton = document.getElementById("aquamarineButton");
 aquamarineButton.addEventListener("click", function(g){  
     q.style.color = "aquamarine";
@@ -28,6 +44,7 @@ aquamarineButton.addEventListener("click", function(g){
     }
 })
 
+//Famlingo
 const flamingoButton = document.getElementById("flamingoButton");
 flamingoButton.addEventListener("click", function(h){
   q.style.color = "rgb(252,142,172)";
@@ -36,6 +53,7 @@ flamingoButton.addEventListener("click", function(h){
   }
 })
 
+//And the Rainbow button!
 const rainbowButton = document.getElementById("rainbowButton");
 rainbowButton.addEventListener("click", function(i){
 
@@ -52,10 +70,28 @@ rainbowButton.addEventListener("click", function(i){
 
 })
 
-let r = document.getElementById(fontSelectP);
+//Identifying the two pieces for the text interaction
+let s = document.getElementById("yourText");
+let t = document.getElementById("changedText")
 
-
-const fontDropdown = document.getElementById("fontselect");
-fontDropdown.addEventListener("change", function(event){
-  r.style.fontFamily = event.target.value;
+//Changing the text to the value of the text box when the button is clicked.
+const inputTextButton = document.getElementById("inputTextButton");
+inputTextButton.addEventListener("click", function(j){
+  
+  t.innerText = s.value;
 })
+
+
+
+//Elements for changing the font
+let r = document.getElementById("fontSelectP");
+const fontDropdown = document.getElementById("fontSelect");
+
+
+//A function to change the font that is called from within the html element, I just wanted to change up my aproach.
+function fontChanger(){
+
+  r.style.fontFamily = fontDropdown.value;
+  r.innerText = "Experimenting with fonts!";
+}
+
